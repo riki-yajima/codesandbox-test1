0d8f1e4288505e8368874dc1f23db353da9c13cf
riki-yajima <94480246+riki-yajima@users.noreply.github.com>
2022-02-22 09:13:20 +0900

    論理演算子

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
 // 分割代入
 // オブジェクト、配列
 
-const myPro = {
-  name: "sato",
-  age: "28"
-};
+// const myPro = {
+//   name: "sato",
+//   age: "28"
+// };
 
-const message1 = `名前は${myPro.name}です。年は${myPro.age}です。`;
-console.log(message1);
+// const message1 = `名前は${myPro.name}です。年は${myPro.age}です。`;
+// console.log(message1);
 
-const { name, age } = myPro;
-const message2 = `名前は${name}です。年は${age}です。`;
-console.log(message2);
+// const { name, age } = myPro;
+// const message2 = `名前は${name}です。年は${age}です。`;
+// console.log(message2);
 
-// デフォルト値、引数
+// // デフォルト値、引数
 
-const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}`);
-sayHello("sato");
+// const sayHello = (name = "ゲスト") => console.log(`こんにちは${name}`);
+// sayHello("sato");
+
+// ||は左側がfalseなら右側を返す
+const num = 100;
+const fee = num || "金額未設定です";
+console.log(fee);
+
+// &&は左側がtrueなら右側を返す
+const num2 = null;
+const fee2 = num2 && "何か設定されました";
+console.log(fee2);
